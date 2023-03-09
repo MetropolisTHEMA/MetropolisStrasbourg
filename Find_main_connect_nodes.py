054368082b235f11bc393c955d77db75db149e5a
thtmkhn <63796705+thtmkhn@users.noreply.github.com>
2023-03-09 09:30:12 +0000

    Upoloading the main connection script
    Main scipt calling funcitons on the 4 other scripts.
    Order :
    1 - Load Data
    2- Connect_OD_to_netwok.py
    3 - Link_main_network.py
    4 - Find_main_connect_nodes.py
    
    Requires shortestpath executable from Metropolis

---
+++ Find_main_connect_nodes.py
@@ -0,0 +1,101 @@
+import os, sys, time
+import json
+from matplotlib import pyplot as plt
+
+from itertools import compress
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import pickle as pk
+#library to replace apply with progress_apply and prompt the progress of the pandas operation
+from tqdm import tqdm
+tqdm.pandas()
+
+
+chronos3=time.time()
+
+def load_results(result):
+      with open(result, 'r') as f:
+        data = json.load(f)
+
+      res = data["results"]
+
+      clean_res = pd.DataFrame(list(filter(None,res)), columns=["id", "travel_time", "route"]).set_index("id")
+      print(f"Warning: discarded {len(res)-len(clean_res) } items ")
+      return clean_res
+
+def find_connections(clean_res, edges, penalty=5):
+      print("Prepare edges data")
+      valid_edges=set(edges[edges["main_network"]]["id"])
+      edges=edges.set_index("id")
+      edge_time=edges.traveltime.to_dict()
+      edge_source=edges.source.to_dict()
+      edge_target=edges.target.to_dict()
+
+      clean_res["id"] = clean_res.index
+
+      print("Find the valid edges on the route")
+      clean_res["main_part"] = clean_res.route.progress_apply(lambda l : [x for x in l if x in valid_edges])
+      print("Mask out the main / residential edges")
+      clean_res["mask"] = clean_res.route.progress_apply(lambda l : [x in valid_edges for x in l ])
+      print("Extract the position of each edges")
+      clean_res["mask"] = clean_res["mask"].progress_apply(lambda l : list(compress(range(len(l)), l)))
+      print("Keep first and last")
+      clean_res["mask"] = clean_res["mask"].progress_apply(lambda l : [l[0],l[-1]] if len(l)!=0 else 0)
+      print("Put trips that never use the main network on the side")
+      no_main = clean_res[clean_res["mask"]==0]
+      print(f"{len(no_main)} trips discarded : no travel on the main network")
+      clean_res = clean_res[clean_res["mask"]!=0]
+
+      print("extract connection edges and nodes into O_edge, D_edge, O_node and D_node columns")
+      clean_res["O_edge"]=clean_res.main_part.progress_apply(lambda l : l[0] )
+      clean_res["D_edge"]=clean_res.main_part.progress_apply(lambda l : l[-1])
+      clean_res["O_node"]=clean_res.O_edge.progress_apply(lambda o : edge_source[o])
+      clean_res["D_node"]=clean_res.D_edge.progress_apply(lambda d : edge_target[d])
+      #clean_res["O_node"]=clean_res.O_edge.progress_apply(lambda o : int(edges[edges["id"]==o]["source"].values) )
+      #clean_res["D_node"]=clean_res.D_edge.progress_apply(lambda d : int(edges[edges["id"]==d]["target"].values))
+      
+      print("Trim the acces trips to the main network, save them on the side")
+      start = clean_res["mask"].apply(lambda l : l[0]).to_dict()
+      finish = clean_res["mask"].apply(lambda l : l[-1]).to_dict()
+      print("start_access:")
+      clean_res["start_access"]=clean_res.progress_apply(lambda l: l.loc["route"][:start[l.loc["id"]]] , axis=1 )
+      print("finish_access:")
+      clean_res["finish_access"]=clean_res.progress_apply(lambda l: l.loc["route"][finish[l.loc["id"]]+1:] , axis=1 )
+      print("main_route:")
+      clean_res["only_main"] = clean_res.progress_apply(lambda l: l.loc["route"][start[l.loc["id"]]:finish[l.loc["id"]]+1] , axis=1 )
+      print("Find how many residential edges are used in the middle of main trips")
+      clean_res["has_residential"] = clean_res.apply( lambda l : len(l.loc["only_main"])-len(l.loc["main_part"]), axis=1)
+
+      print("Compute access times")
+      print("start_access_time:")
+      clean_res["start_access_time"] = clean_res.progress_apply(lambda l : np.sum([edge_time[x]+penalty for x in l.loc["start_access"]]), axis=1)
+      print("finish_access_time:")
+      clean_res["finish_access_time"] = clean_res.progress_apply(lambda l : np.sum([edge_time[x]+penalty for x in l.loc["finish_access"]]), axis=1)
+
+      # clean_res["main_connect_nodes"]=clean_res.progress_apply(lambda l: [edge_source[l["mask"][0]],edge_target[l["mask"][-1]]], axis=1)
+      return clean_res, no_main
+
+
+if __name__=="__main__":
+      
+      chronos3=time.time()
+
+      outdir = "strasbourg_outputs/Shortest_Path"
+
+      edges = pk.load(open(os.path.join(outdir,"graph_edges"), "rb")).set_index("id")
+
+      result=os.path.join(outdir,"output.json")  
+
+      clean_res = load_results(result)
+
+      clean_res, no_main = find_connections(clean_res, edges)
+
+      count_O=clean_res[["route","O_node"]].groupby("O_node").count()
+      count_D=clean_res[["route","D_node"]].groupby("D_node").count()
+
+      connectors=set(count_O.index.append(count_D.index))
+
+      chronos3=time.time()-chronos3
+      print(f"Le script 3 a mis {chronos3/60} minutes à tourner")
+      
---
+++ Link_main_network.py
@@ -0,0 +1,208 @@
+import os, sys, time
+import json
+import subprocess
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import pickle as pk
+from shapely.geometry import Point, LineString
+
+p="paris_"
+
+chronos2=time.time()
+
+# Input files
+tripfile = p+"outputs/OD_to_network/trips_OD"
+edgefile = p+"outputs/graphbuilder/raw_edges"
+nodefile = p+"outputs/graphbuilder/raw_nodes"
+#output directory
+outdir = p+"outputs/shortestspath"
+if not os.path.isdir(outdir):
+    os.makedirs(outdir)
+
+#use the right projection for metric operations
+METRIC_CRS = "EPSG:2154"
+
+# Script location
+scriptdir = r"C:\Users\theot\OneDrive\TheseMaster\Datapipeline\ShortestPath"
+TCHscript = "compute_travel_times"
+
+
+# Parametrer file: parameters.json -> write manually
+
+parameters = {
+  "algorithm": "TCH",
+  "output_route": True,
+}
+
+
+#Dir check
+
+#Main
+
+
+# #load edges 
+# print("Reading edges")
+# try:
+#     edges = pk.load(open(edgefile,"rb"))
+#     edges.crs = METRIC_CRS
+#     edges.set_geometry("geometry", inplace = True)
+# except: 
+#     edges = gpd.read_file(edgefile+"geojson").to_crs(METRIC_CRS)
+#     edges.to_pickle(edgefile)
+
+#load nodes
+def prepare_inputs(outdir, tripfile, edgefile, nodefile, save=False, crs=METRIC_CRS):
+    print("Reading edges node")
+    try:
+        edges = pk.load(open(os.path.join(outdir,"graph_edges"),"rb"))
+        nodes = pk.load(open(os.path.join(outdir,"graph_nodes"),"rb"))
+        trips = pk.load(open(os.path.join(outdir,"graph_trips"),"rb"))
+    except: 
+        print("Reindexing needed")
+
+        print("Reading trips")
+        try:
+            trips = pk.load(open(tripfile,"rb"))
+            trips = pd.DataFrame(trips)
+        except: 
+            trips = pd.read_csv(tripfile+".csv")
+        
+        print("Reading edges")
+        try:
+            edges = pk.load(open(edgefile,"rb"))
+            edges.crs = METRIC_CRS
+            edges.set_geometry("geometry", inplace = True)
+        except: 
+            edges = gpd.read_file(edgefile+"geojson").to_crs(METRIC_CRS)
+            edges.to_pickle(edgefile)
+
+        print("Reading nodes")
+        try:
+            nodes = pk.load(open(nodefile,"rb"))
+            nodes.crs = METRIC_CRS
+            nodes.set_geometry("geometry", inplace = True)
+        except: 
+            nodees = gpd.read_file(nodefile+"geojson").to_crs(METRIC_CRS)
+            nodes.to_pickle(nodefile)
+
+        # Removing duplicate edges.
+        st_count = edges.groupby(['source', 'target'])['id'].count()
+        to_remove = set()
+        for s, t in st_count.loc[st_count > 1].index:
+            dupl = edges.loc[(edges['source'] == s) & (edges['target'] == t)]
+            # Keep only the edge with the smallest travel time.
+            tt = dupl['length'] / (dupl['speed'] / 3.6)
+            id_min = tt.index[tt.argmin()]
+            for i in dupl.index:
+                if i != id_min:
+                    to_remove.add(i)
+        if to_remove:
+            print('Warning. Removing {} duplicate edges.'.format(len(to_remove)))
+            edges.drop(labels=to_remove, inplace=True)
+                
+        # Node ande edge ids should start at 0.
+        print("Index reset")
+        nodes.reset_index(drop=True, inplace=True)
+        edges.reset_index(drop=True, inplace=True)
+        edges["id"]=edges.index
+        #edges.drop(columns="id", inplace=True)
+        node_id_map = nodes["id"].to_frame().reset_index().set_index("id")
+        nodes["id"]=nodes.index
+        edges = edges.merge(node_id_map, left_on="source", right_index=True).drop(columns=["source"]).rename(
+            columns={"index": "source"}
+        )
+        edges = edges.merge(node_id_map, left_on="target", right_index=True).drop(columns=["target"]).rename(
+            columns={"index": "target"}
+        ).sort_index()
+        trips = trips.merge(node_id_map, left_on="O_connect", right_index=True).drop(columns=["O_connect"]).rename(
+            columns={"index": "O_connect"}
+        )
+        trips = trips.merge(node_id_map, left_on="D_connect", right_index=True).drop(columns=["D_connect"]).rename(
+            columns={"index": "D_connect"}
+        ).sort_index()
+
+
+        edges["traveltime"]=(edges["length"]/edges["speed"])*3600
+        
+        print("Save files")
+        edges.to_crs(METRIC_CRS, inplace=True)
+        if save:
+            edges.to_file(os.path.join(outdir,"graph_edges.geojson"), driver="GeoJSON")
+        edges.to_pickle(os.path.join(outdir,"graph_edges"))
+
+        nodes.to_crs(METRIC_CRS, inplace=True)
+        if save:
+            nodes.to_file(os.path.join(outdir,"graph_nodes.geojson"), driver="GeoJSON")
+        nodes.to_pickle(os.path.join(outdir,"graph_nodes"))
+
+        trips.to_pickle(os.path.join(outdir,"graph_trips"))
+    return trips, edges, nodes
+
+
+def prepare_shortestpath(outdir, trips, edges, parameters=parameters):
+    print("Setting query")
+    trips[['departime']]=0
+    query = trips[["trip_index","O_connect","D_connect","departime"]].values.tolist()
+    print("Creating Graph")
+    # c = time.time()
+    # graph = edges.apply(lambda row :[ row["source"], row["target"], row["traveltime"]], axis=1)
+    # c = time.time()-c
+
+    # graph = edges[["source","target","traveltime"]].tolist()
+    e = edges[["source","target","traveltime"]]
+    graph = [*map(list, zip(*map(e.get, e)))]
+
+    print("Writing data...")
+    print("Graph")
+    with open(outdir+"/graph.json", "w") as f:
+        f.write(json.dumps(graph))
+    print("Parameters")
+    with open(outdir+"/parameters.json", "w") as f:
+        f.write(json.dumps(parameters))
+    print("Queries")
+    with open(outdir+"/queries.json", "w") as f:
+        f.write(json.dumps(query))
+    
+    print("Done!")
+
+
+def run_shortestpath(outdir, scriptdir, TCHscript):
+    
+    #Inputs of the shortestpath script
+
+            #Path to the file where the queries to compute are stored
+    q=  f"--queries  {outdir}/queries.json "
+            #Path to the file where the graph is stored
+    g=  f"--graph {outdir}/graph.json "
+            #Path to the file where the results of the queries should be stored
+    o=  f"--output {outdir}/output.json "
+            #Path to the file where the parameters are stored 
+    p=  f"--parameters {outdir}/parameters.json "
+
+    #Run TCH script
+    print("Run TCH script")
+    #add ".exe" if on windows
+    syst = ""
+    if sys.platform == "win32":
+        syst=".exe"
+
+
+    command = f"{os.path.join(scriptdir,TCHscript)}{syst} {q} {p} {o} {g} " 
+
+    subprocess.run(command, shell=True)
+    print(f"Saved in {outdir}")
+
+if __name__=="__main__":
+    chronos2=time.time()
+
+    trips, edges = prepare_inputs(outdir, tripfile, edgefile, nodefile, crs=METRIC_CRS)
+
+    prepare_shortestpath(trips, edges, parameters=parameters)
+
+    run_shortestpath(outdir, scriptdir, TCHscript)
+
+    chronos2=time.time()-chronos2
+    print(f"Le script a mis {chronos2/60} minutes à tourner")
+
