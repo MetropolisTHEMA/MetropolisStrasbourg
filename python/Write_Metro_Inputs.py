3c673ddebe79cab8bbfe06d7d197679592e761a9
thtmkhn <63796705+thtmkhn@users.noreply.github.com>
2023-03-09 17:18:18 +0000

    Updated main file and associated scipts

---
+++ python/Write_Metro_Inputs.py
@@ -0,0 +1,293 @@
+#%% IMPORTS
+import os
+import json
+
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+
+#%% PARAMETERS
+# Path to the directory where the node and edge files are stored.
+ROAD_NETWORK_DIR = "./output/osm_network/"
+# Path to the file where the trip data is stored.
+TRIPS_FILE = "./output/trips_filtered.csv"
+# Path to the directory where the simulation input should be stored.
+OUTPUT_DIR = "./output/next_run/"
+# Vehicle length in meters.
+VEHICLE_LENGTH = 10.0 * 10.0
+# Vehicle passenger-car equivalent.
+VEHICLE_PCE = 10.0 * 1.0
+# Period in which the departure time of the trip is chosen.
+PERIOD = [3.0 * 3600.0, 10.0 * 3600.0]
+# Capacity of the different edge road types.
+CAPACITY = {
+    0: None,
+    1: 2000,
+    2: 2000,
+    3: 1500,
+    4: 800,
+    5: 600,
+    6: 600,
+    7: 600,
+    8: 1500,
+    9: 1500,
+    10: 1500,
+    11: 800,
+    12: 600,
+    13: 300,
+    14: 300,
+    15: 300,
+}
+# If True, enable entry bottleneck using capacity defined by `CAPACITY`.
+ENTRY_BOTTLENECK = True
+# If True, enable exit bottleneck using capacity defined by `CAPACITY`.
+EXIT_BOTTLENECK = False
+# Value of time in the car, in euros / hour.
+ALPHA = 15.0
+# Value of arriving early at destination, in euros / hour.
+BETA = 7.5
+# Value of arriving late at destination, in euros / hour.
+GAMMA = 30.0
+# Time window for on-time arrival, in seconds.
+DELTA = 0.0
+# If True, departure time is endogenous.
+ENDOGENOUS_DEPARTURE_TIME = True
+# Value of μ for the departure-time model (if ENDOGENOUS_DEPARTURE_TIME is True).
+DT_MU = 3.0
+# How t* is computed given the observed arrival time.
+def T_STAR_FUNC(ta):
+    return ta
+CONST_TT = {
+    3: 10,
+    4: 15,
+    5: 20,
+    6: 25,
+    7: 30,
+    8: 35,
+    9: 40,
+    10: 45,
+}
+# Seed for the random number generators.
+SEED = 13081996
+RNG = np.random.default_rng(SEED)
+
+
+# Parameters to use for the simulation.
+PARAMETERS = {
+    "period": PERIOD,
+    "init_iteration_counter": 1,
+    "learning_model": {
+        "type": "Exponential",
+        "value": {
+            "alpha": 0.99,
+        },
+    },
+    "stopping_criteria": [
+        {
+            "type": "MaxIteration",
+            "value": 5,
+        },
+    ],
+    "update_ratio": 1.0,
+    "random_seed": SEED,
+    "network": {
+        "road_network": {
+            "recording_interval": 300.0,
+            "simulated_simplification": {
+                "type": "Bound",
+                "value": 2.0,
+            },
+            "weight_simplification": {
+                "type": "Bound",
+                "value": 2.0,
+            },
+            "overlay_simplification": {
+                "type": "Bound",
+                "value": 2.0,
+            },
+            "search_space_simplification": {
+                "type": "Bound",
+                "value": 1.0,
+            },
+        }
+    },
+}
+
+#{
+# # Removing duplicate edges.
+# st_count = edges.groupby(['source_index', 'target_index'])['index'].count()
+# to_remove = set()
+# for s, t in st_count.loc[st_count > 1].index:
+#     dupl = edges.loc[(edges['source_index'] == s) & (edges['target_index'] == t)]
+#     # Keep only the edge with the smallest travel time.
+#     tt = dupl['length'] / (dupl['speed'] / 3.6)
+#     id_min = tt.index[tt.argmin()]
+#     for i in dupl.index:
+#         if i != id_min:
+#             to_remove.add(i)
+# if to_remove:
+#     print('Warning. Removing {} duplicate edges.'.format(len(to_remove)))
+#     edges.drop(labels=to_remove, inplace=True)
+#}
+#%% GRAPH
+def generate_road_network(edges):
+    print("Creating Metropolis road network")
+    metro_edges = list()
+    for _, row in edges.iterrows():
+        edge = [
+            row["source"],
+            row["target"],
+            {
+                "id": int(row["id"]),
+                "base_speed": float(row["speed"]) / 3.6,
+                "length": float(row["length"]),
+                "lanes": int(row["lanes"]),
+                "speed_density": {
+                    "type": "FreeFlow",
+                },
+            },
+        ]
+        if capacity := CAPACITY.get(row["road_type"]):
+            if ENTRY_BOTTLENECK:
+                edge[2]["bottleneck_inflow"] = capacity / 3600.0
+            if EXIT_BOTTLENECK:
+                edge[2]["bottleneck_outflow"] = capacity / 3600.0
+        if const_tt := CONST_TT.get(row["neighbor_count"]):
+            edge[2]["constant_travel_time"] = const_tt
+        metro_edges.append(edge)
+
+    graph = {
+        "edges": metro_edges,
+    }
+
+    vehicles = [
+        {
+            "length": VEHICLE_LENGTH,
+            "pce": VEHICLE_PCE,
+            "speed_function": {
+                "type": "Base",
+            },
+        }
+    ]
+
+    road_network = {
+        "graph": graph,
+        "vehicles": vehicles,
+    }
+    return road_network
+
+#%% AGENTS
+def generate_agents(trips):
+    print("Generating agents")
+    random_u = iter(RNG.uniform(size=len(trips)))
+    agents = list()
+    maxp = trips.person_id.max()
+    maxt = trips.id.max()
+    for person_id, idx in trips.groupby('person_id').groups.items():
+        legs = list()
+        prev_ta = None
+        for key, trip in trips.loc[idx].iterrows():
+            print(f"person_id : {person_id}/{maxp}    trip: {trip['id']}/{maxt}", end="\r")
+                    
+            t_star = T_STAR_FUNC(trip["arrival_time"])
+            
+            if trip["road_leg"]:
+                leg={"class" : {
+                    "type": "Road",
+                    "value": {
+                        "origin": trip['O_node'],
+                        "destination": trip['D_node'],
+                        "vehicle": 0,
+                            }
+                    }}
+                leg["travel_utility"]= {
+                    "type": "Polynomial",
+                    "value": {
+                        "b": -ALPHA / 3600.0,
+                    }
+                }                
+                leg["schedule_utility"]= {
+                    "type": "AlphaBetaGamma",
+                    "value": {
+                        "beta": BETA / 3600.0,
+                        "gamma": GAMMA / 3600.0,
+                        "t_star_high": t_star + DELTA / 2.0 - trip["D_access_time"],
+                        "t_star_low": t_star - DELTA / 2.0 - trip["D_access_time"],
+                        }
+                    }
+                if not prev_ta is None:
+                    # Set stopping time of previous leg.
+                    legs[-1]["stopping_time"] = trip['departure_time'] - prev_ta + trip["O_access_time"]
+                prev_ta = trip['arrival_time'] - trip["D_access_time"]
+                
+            else:
+                leg = {
+                    "class": {
+                    "type": "Virtual",
+                    "value": trip["travel_time_sp"]} 
+                    }             
+                if not prev_ta is None:
+                    # Set stopping time of previous leg.
+                    legs[-1]["stopping_time"] = trip['departure_time'] - prev_ta
+                prev_ta = trip['arrival_time']
+            legs.append(leg)
+        if ENDOGENOUS_DEPARTURE_TIME:
+            departure_time_model = {
+                "type": "ContinuousChoice",
+                "value": {
+                    "period": PERIOD,
+                    "choice_model": {
+                        "type": "Logit",
+                        "value": {
+                            "u": next(random_u),
+                            "mu": DT_MU,
+                        },
+                    },
+                },
+            }
+        else:
+            departure_time_model = {
+                "type": "Constant",
+                "value": trips.loc[idx[0], "departure_time"],
+            }
+        car_mode = {
+            "type": "Trip",
+            "value": {
+                "legs": legs,
+                "departure_time_model": departure_time_model,
+            },
+        }
+        agent = {
+            "id": person_id,
+            "modes": [car_mode],
+        }
+        agents.append(agent)
+    print("\n Done !")
+    return agents
+#%% WRITER
+def write_data(OUTPUT_DIR,road_network,agents,PARAMETERS=PARAMETERS):
+    print("Writing data...")
+    if not os.path.isdir(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
+    print("network")
+    with open(os.path.join(OUTPUT_DIR, "network.json"), "w") as f:
+        f.write(json.dumps(road_network))
+    print("agents")
+    with open(os.path.join(OUTPUT_DIR, "agents.json"), "w") as f:
+        f.write(json.dumps(agents))
+    print("parameters")
+    with open(os.path.join(OUTPUT_DIR, "parameters.json"), "w") as f:
+        f.write(json.dumps(PARAMETERS))
+#%%
+if __name__ == "__main__":
+        
+    print("Reading edges")
+    edges = gpd.read_file(os.path.join(ROAD_NETWORK_DIR, "edges.fgb"))
+    
+    print("Reading trips")
+    trips = pd.read_csv(TRIPS_FILE)
+
+    road_network = generate_road_network(edges[edges["main_network"]])
+
+
+# %%
---
+++ python/main.py
@@ -0,0 +1,223 @@
+#%% 
+# IMPORT SCRIPTS
+import os, sys, time
+#base libraries
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+import pickle as pk
+#scrpit functions
+import Load_data as script0
+import Connect_OD_to_network as script1
+import Link_main_network as script2
+import Find_main_connect_nodes as script3
+import Write_Metro_Inputs as script4
+
+#add progress_apply to pandas for more readbility in code running
+from tqdm import tqdm
+tqdm.pandas()
+#disable annoying warning forcing .loc
+pd.options.mode.chained_assignment = None  # default='warn'
+# if you want to plt.show thing in command run
+from matplotlib import pyplot as plt
+
+
+#%% PARAMETERS
+#Input files
+OSM_FILE= r"C:\Users\theot\OneDrive\TheseMaster\Datapipeline\alsace-latest.osm.pbf"
+OD_FILE= r"C:\Users\theot\OneDrive\TheseMaster\Data\Equasim\output_100%\strasbourg_trips.gpkg"
+
+#Outputs
+output_folder = "FINAL_strasbourg_outputs"
+
+#Map Projections
+METRIC_CRS="EPSG:2154"
+WORLD_CRS="EPSG:4326"
+
+#Shortest path executable
+scriptdir = r"C:\Users\theot\OneDrive\TheseMaster\Datapipeline\ShortestPath"
+TCHscript = "compute_travel_times"
+
+# all dirs
+
+LOAD_DIR = os.path.join(output_folder,"OSM_to_edges_nodes")
+if not os.path.isdir(LOAD_DIR):
+    os.makedirs(LOAD_DIR)
+OD_NETWORK_DIR = os.path.join(output_folder,"OD_to_network")
+if not os.path.isdir(OD_NETWORK_DIR):
+    os.makedirs(OD_NETWORK_DIR)
+SHORT_PATH_DIR = os.path.join(output_folder,"Shortest_Path")
+if not os.path.isdir(SHORT_PATH_DIR):
+    os.makedirs(SHORT_PATH_DIR)
+METRO_INPUT_DIR = os.path.join(output_folder,"Metro_Input")
+if not os.path.isdir(METRO_INPUT_DIR):
+    os.makedirs(METRO_INPUT_DIR)
+
+NODE_FILE = os.path.join(LOAD_DIR,"raw_nodes")
+EDGE_FILE = os.path.join(LOAD_DIR,"raw_edges")
+TRIP_FILE=os.path.join(OD_NETWORK_DIR,"trips_residential_connected")
+
+#set log
+log = {}
+
+#%% 0
+# SCRIPT 0: Load_OSM_Data
+chronos=time.time()
+chronos0=time.time()
+# File does not exists or is not in the same folder as the script.
+if not os.path.exists(OSM_FILE):
+    print("File not found: {}".format(OSM_FILE))
+    sys.exit(0)
+
+h = script0.NodeReader()
+
+print("Finding nodes...")
+h.apply_file(OSM_FILE, locations=True, idx="flex_mem")
+
+g = script0.Writer(h.nodes)
+
+print("Reading OSM data...")
+g.apply_file(OSM_FILE, locations=True, idx="flex_mem")
+
+print("Post-processing...")
+g.post_process(simplify=False)
+
+print("Found {} nodes and {} edges.".format(len(g.nodes), len(g.edges)))
+
+print("Writing edges...")
+g.write_edges(EDGE_FILE)
+
+print("Writing nodes...")
+g.write_nodes(NODE_FILE)
+
+print("Done!")
+
+log["script 0 nodes count"] = len(g.nodes)
+log["script 0 edges count"] = len(g.edges)
+
+chronos0=time.time()-chronos0
+print(f"Le script 0 a mis {chronos0/60} minutes à tourner")
+
+#%% 1
+# SCRIPT 1: Connect_OD_to_full_network
+chronos1=time.time()
+
+
+CONNECTED_ROADTYPES = {
+    #"motorway": 1,
+    #"trunk": 2,
+    "primary": 3,
+    "secondary": 4,
+    "tertiary": 5,
+    "unclassified": 6,
+    "residential": 7,
+    #"motorway_link": 8,
+    #"trunk_link": 9,
+    "primary_link": 10,
+    "secondary_link": 11,
+    "tertiary_link": 12,
+    "living_street": 13,
+    "road": 14,
+    "service": 15,
+}
+
+trips,O,D = script1.prepare_trips(OD_FILE,OD_NETWORK_DIR)
+edges = script1.read_edges(EDGE_FILE, LOAD_DIR)
+
+log = {}
+
+
+log["script 1 start trips count"]=len(trips)
+log["script 1 finish trips count"]=len(edges)
+
+nearO = script1.nearjoin(O, edges, OD_NETWORK_DIR+"Originset", CONNECTED_ROADTYPES, save=False )
+nearD = script1.nearjoin(D, edges, OD_NETWORK_DIR+"Destinationset", CONNECTED_ROADTYPES, save=False )
+
+trips=trips.join(nearO[["connect","connect_dist","edge_dist"]].add_prefix("O_"), how="inner", on="trip_index")
+trips=trips.join(nearD[["connect","connect_dist","edge_dist"]].add_prefix("D_"), how="inner", on="trip_index")
+
+#delete trips with same O and D
+trips=trips[trips["O_connect"]!=trips["D_connect"]]
+
+#trips.to_csv(OUTDIR_1+"trips_OD.csv")
+trips.to_pickle(os.path.join(OD_NETWORK_DIR,"trips_residential_connected"))
+
+
+log["script 1 finish trips count"]=len(trips)
+
+chronos1=time.time()-chronos1
+print(f"Le script 1 a mis {chronos1/60} minutes à tourner")
+
+#%% 2
+# SCRIPT 2 : Link_full_to_main
+
+chronos2=time.time()
+
+trips, edges, nodes = script2.prepare_inputs(SHORT_PATH_DIR, TRIP_FILE, EDGE_FILE, NODE_FILE, save=True ,crs=METRIC_CRS)
+
+
+log["script 2 start trips count"]=len(trips)
+log["script 2 start edges count"]=len(edges)
+log["script 2 start nodes count"]=len(nodes)
+
+
+
+script2.prepare_shortestpath(SHORT_PATH_DIR, trips, edges)
+
+script2.run_shortestpath(SHORT_PATH_DIR, scriptdir, TCHscript)
+
+chronos2=time.time()-chronos2
+print(f"Le script 2 a mis {chronos2/60} minutes à tourner")
+
+
+#%% 3
+# SCRIPT 3 : Find_main_bounday_node
+
+chronos3=time.time()
+RESULTS = os.path.join(SHORT_PATH_DIR,"output.json")
+
+
+res = script3.load_results(RESULTS)
+
+res, no_main = script3.find_connections(res, edges)
+
+res.to_pickle(os.path.join(SHORT_PATH_DIR,"treated_results"))
+no_main.to_pickle(os.path.join(SHORT_PATH_DIR,"side_results"))
+
+trips = script3.final_trip_file(res,no_main, trips)
+
+trips.to_pickle(os.path.join(SHORT_PATH_DIR,"final_results"))
+
+
+chronos3=time.time()-chronos3
+print(f"Le script 3 a mis {chronos3/60} minutes à tourner")
+#%% 4
+# Script 4: writing_metro_inputs
+
+chronos4=time.time()
+# res = pk.load(open(os.path.join(OUTDIR_2,"treated_results"),"rb"))
+# no_main=pk.load(open(os.path.join(OUTDIR_2,"side_results"),"rb"))
+# trips= pk.load(open(os.path.join(SHORT_PATH_DIR,"final_results"),"rb"))
+main_edges = edges[edges["main_network"]]
+
+road_network = script4.generate_road_network(main_edges)
+agents = script4.generate_agents(trips)
+
+script4.write_data(METRO_INPUT_DIR,road_network, agents)
+
+chronos4=time.time()-chronos4
+
+
+
+#%% 
+# Plot performances
+print(f"Le script 0 a mis {chronos0/60} minutes à tourner")
+print(f"Le script 1 a mis {chronos1/60} minutes à tourner")
+print(f"Le script 2 a mis {chronos2/60} minutes à tourner")
+print(f"Le script 3 a mis {chronos3/60} minutes à tourner")
+print(f"Le script 4 a mis {chronos3/60} minutes à tourner")
+chronos=time.time()-chronos
+print(f"Le total des scripts ont mis {chronos/60} minutes à tourner")
+
+
+# %%
